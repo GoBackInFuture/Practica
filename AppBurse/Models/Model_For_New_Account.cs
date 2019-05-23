22fe684f941e2b65019f287135892957f2d2569c
andreicomaneci <48902437+andreicomaneci@users.noreply.github.com>
2019-05-23 09:06:03 +0300

    Add model_for_new_account

---
+++ AppBurse/Models/Model_For_New_Account.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppBurse.Models
+{
+    public class Model_For_New_Account
+    {
+        public RegisterViewModel user_to_register { get; set; }
+        public IEnumerable<SelectListItem> AllRoles { get; set; }
+    }
+}
